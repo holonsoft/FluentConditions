7851a7867a85a14225dfdb3bc8da672ad891383f
Alexander Bülow <alexander.buelow.ext@zeiss.com>
2021-05-30 20:04:59 +0200

    tests

---
+++ holonsoft.FluentConditions.Tests/TestReferenceTypes.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using System;
+using Xunit;
+
+namespace holonsoft.FluentConditions.Tests
+{
+	public class TestReferenceTypes
+	{
+		[Fact]
+		public void TestNotNull()
+		{
+			object obj = new object();
+
+			Action requireAction
+				= () => obj.Requires(nameof(obj))
+									 .IsNotNull();
+
+			requireAction.Should().NotThrow();
+
+			obj = null;
+
+			requireAction.Should().Throw<ArgumentNullException>();
+		}
+
+		[Fact]
+		public void TestNull()
+		{
+			object obj = null;
+
+			Action requireAction
+				= () => obj.Requires(nameof(obj))
+									 .IsNull();
+
+			requireAction.Should().NotThrow();
+
+			obj = new object();
+
+			requireAction.Should().Throw<ArgumentNullException>();
+		}
+	}
+}
